1e0b0acea18307f8b7515d857fe2fd3c0dd58286
Matthew Van Huffel <55860096+Mvanhuffel@users.noreply.github.com>
2024-01-26 17:42:11 +0700

    Update and rename Automate Datetime Updates.sql to automate_datetime_update.sql

--- Postgres Stored Procedure/automate_datetime_update.sql
+++ Postgres Stored Procedure/automate_datetime_update.sql
@@ -1,13 +1,3 @@
-/*
-When working with databases, it's important to keep track of when data is updated or inserted. 
-One common way to do this is to have a column in your table that records the time of the last update. 
-However, manually updating this column every time you insert or update data can be tedious and error-prone. 
-That's where stored procedures and triggers come in.
-
-Here is an example that sets up a trigger to automatically update the datetime column of a row whenever 
-an update operation is performed on the table. The stored procedure is used to define the functionality of the trigger.
-*/
-
 --table
 CREATE TABLE transaction (
     id SERIAL PRIMARY KEY,
